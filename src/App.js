0450aeda1a17f047318fb6dc23b8d7998d8c4e17
Manuel Chouza <316813@twgroup.co.nz>
2024-04-26 13:56:44 +0200

    updated

--- src/App.js
+++ src/App.js
@@ -41,6 +41,32 @@ function App() {
             <h3 className='custom-h3'>Feel fre to ask me any question or consultation you might have at: 017674676326</h3>
           </Col>
         </Row>
+        <Row className="spacer"></Row>
+        <Row>
+          <Col xs={6}>
+            <h3 className='about'>FAQ</h3>
+          </Col>
+          <Col xs={6}>
+            <h3 className='custom-h3'>
+              Starnberg club is the only club that partitions the time in slots of 45 min, 
+              therefore you can get some benefits by paying smaller slots to improve very specific feature per slot.<br/>
+              Alternatively, we ca always use other facilities as the weather and seasons of the year progress, 
+              for an instance  an indoor facility might well be required during winter.<br/>
+              <br/>
+              <b>What shoes to wear:</b> Any Tennis or Padel shoes will be ok.<br/>
+              <b>What shoes not to wear:</b> Any trekking or hiking shoes :) 
+              <br/>
+              <b>How to book my time:</b> You can contact me via whatsApp or text message.
+              <br/>
+              <b>What to mention in your first message or introduction to me: </b><br/>
+              * Whether you have never plaied Padel before<br/>
+              * What time of the day is your preference, [morning, afternoon or evenings(after 5pm)]<br/>
+              * Preference of location of the courts: <a href='https://maps.app.goo.gl/hvPj1EbxmHwUwvrC6'>Scheck</a>, <a href='https://maps.app.goo.gl/ZY98xuKTXVJa8p7y6'>Starnberg</a>, <a href='https://maps.app.goo.gl/5YWM6bfQ4ktGTDFfA'>Padel City</a><br/>
+              * Whether you have a friend or partner to whom you will be taken lessons with<br/>
+              * Whether you want to take lessons by yourself only.
+            </h3>
+          </Col>
+        </Row>
       </Container>
     </div>
   );
